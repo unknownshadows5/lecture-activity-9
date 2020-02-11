95a1455909d08cca593067219c8c898b74cc51aa
Johnson <jamesjohnson8@my.whccd.edu>
2020-02-10 20:20:56 -0800

    finished the activity

---
+++ script.js
@@ -0,0 +1,22 @@
+let namesofpeople = ["Brad", "Danney", "Dan", "Bradly", "asheely"];
+
+
+
+for (let i = 0; i <= namesofpeople.length - 1;i++ ) {
+  
+  console.log(namesofpeople[i]);
+
+}
+
+namesofpeople.push("carry")
+namesofpeople.push("bill")
+namesofpeople.push("marry")
+namesofpeople.push("carl")
+namesofpeople.push("barry")
+console.log(namesofpeople)
+
+namesofpeople.pop()
+namesofpeople.pop()
+namesofpeople.pop()
+
+console.log(namesofpeople.length)
